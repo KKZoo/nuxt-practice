42a1308abedb3574c44aef4497b53cb9a77b0111
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2020-08-11 16:14:04 +0900

    axiosを使ってみた

--- components/page.vue
+++ components/page.vue
@@ -5,6 +5,7 @@
         </div>
         <router-link to="/">Top</router-link>
         <router-link to="second-page">second-page</router-link>
+        <router-link to="axios">axios</router-link>
     </section>
 </template>>
 
---
+++ pages/axios.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="container">
+        <li v-for="user in users" v-bind:key="user.id">
+            {{ user.id }}, {{ user.name }}
+        </li>
+    </section>
+</template>>
+
+<script>
+const axios = require('axios')
+let url = "https://jsonplaceholder.typicode.com/users"
+
+export default {
+    asyncData({ params }) {
+        return axios.get(url)
+            .then(function (response) {
+                return { users: response.data }
+            console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+            console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
+}
+</script>>
